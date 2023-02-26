02aea1ff8ae2e9d59d4b88029e739f57cfef4597
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2023-02-26 02:12:57 +0100

    testimonial section

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,8 +1,76 @@
 import './portfolio.css'
-
+import Img1 from '../../assets/portfolio1.jpg';
+import Img2 from '../../assets/portfolio2.jpg';
+import Img3 from '../../assets/portfolio3.jpg';
+import Img4 from '../../assets/portfolio4.jpg';
+import Img5 from '../../assets/portfolio5.png';
+import Img6 from '../../assets/portfolio6.jpg';
+const data=[
+  {
+    id:1,
+    image:Img1,
+    title:"Crypto Currency Dashboard & Financial Visualisation",
+    githun:'https://github.com/MohamedAminebenhmida',
+    demo:'https://github.com/MohamedAminebenhmida'
+  },
+   {
+    id:2,
+    image:Img2,
+    title:"Charts templates & infographics in Figma",
+    githun:'https://github.com/MohamedAminebenhmida',
+    demo:'https://github.com/MohamedAminebenhmida'
+  }, 
+  {
+    id:3,
+    image:Img3,
+    title:"Figma Dashboard UI kit for data design",
+    githun:'https://github.com/MohamedAminebenhmida',
+    demo:'https://github.com/MohamedAminebenhmida'
+  }, 
+  {
+    id:4,
+    image:Img4,
+    title:"Maintaining tasks and tracking progress",
+    githun:'https://github.com/MohamedAminebenhmida',
+    demo:'https://github.com/MohamedAminebenhmida'
+  }, 
+  {
+    id:5,
+    image:Img5,
+    title:"Charts templates & infographics in Figma",
+    githun:'https://github.com/MohamedAminebenhmida',
+    demo:'https://github.com/MohamedAminebenhmida'
+  }, 
+  {
+    id:6,
+    image:Img6,
+    title:"Crypto Currency Dashboard & Financial Visualisation",
+    githun:'https://github.com/MohamedAminebenhmida',
+    demo:'https://github.com/MohamedAminebenhmida'
+  }, 
+]
 const Portfolio = () => {
   return (
-    <section id="experience">Portfolio</section>
+    <section id="portfolio">
+      <div className="portfolio-item">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      </div>
+      <div className="container portfolio__container">
+        {data.map(({id,image,title,github,demo})=>{
+          return(
+        <article className="portfolio__item" key={id}>
+        <div className="portfolio__item-image">
+          <img src={image} alt={title}/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href={github} className='btn dark'>Github</a>
+          <a href={demo} className='btn prple' target='_blank' rel='noreferrer'> Demo</a>  </div>            
+        </article>)})}
+
+      </div>
+    </section>
   )
 }
 
--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
-
+import './testimonials.css'
+import Avatar1 from '../../assets/avatar1.jpg';
+import Avatar2 from '../../assets/avatar2.jpg';
+import Avatar3 from '../../assets/avatar3.jpg';
+import Avatar4 from '../../assets/avatar4.jpg';
+import {Pagination,Navigation} from 'swiper'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/pagination';
+const data=[
+{
+  id:1,
+  avatar:Avatar1,
+  name:"Tina Snow",
+  review:"Bad !"
+},
+{
+  id:2,
+  avatar:Avatar2,
+  name:"Shatta Walle",
+  review:"Really Impressive"
+},
+{
+  id:3,
+  avatar:Avatar3,
+  name:"Emilia Stark",
+  review:"Class"
+},
+{
+  id:4,
+  avatar:Avatar4,
+  name:"John Snow",
+  review:"Addicted to Your Work"
+},
+]
 const Testimonials = () => {
   return (
-    <section id="testimonials"><h2>Testimonials</h2></section>
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[ Pagination,Navigation]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {data.map(({id,avatar,name,review})=>{
+          return(
+ <SwiperSlide className="testimonial" key={id}>
+          <div className="client__avatar">
+            <img src={avatar} alt=''/>
+            </div>
+            <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>
+              {review}
+            </small>  
+        </SwiperSlide>  
+        )})}
+      </Swiper>
+      </section>
   )
 }
 
