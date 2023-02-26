33d8ed6e3b3ea19c5d560a6643c49b3d39c5a7a5
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2023-02-26 15:42:34 +0100

    footer section added

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,17 @@
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md';
 import {RiMessengerLine} from 'react-icons/ri';
 import {BsWhatsapp} from 'react-icons/bs';
 const Contact = () => {
+  const form=useRef();
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_lu6rt1q', 'template_8n0wbwr', form.current, 'rRDe-qiQIw-x1X6j7');
+    e.target.reset()
+  };
   return (
     <section id="contact">
     <div className="contact-item">
@@ -15,7 +24,7 @@ const Contact = () => {
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h6>mohamedaminebenhmida4@gmail.com</h6>
-            <a href='mailto:mohamedaminebenhmida4@gmail.com' target='_blank' rel='noreferrer'>Send A message</a>
+            <a href='mailto:mohamedaminebenhmida4@gmail.com' target='_blank' rel='noreferrer' className>Send A message</a>
           </article>
           <article className='contact__option'>
             <RiMessengerLine contact__option-icon/>
@@ -30,7 +39,7 @@ const Contact = () => {
             <a href='https://api.whatsapp.com/send?phone=+21652982300' target='_blank' rel='noreferrer'>Send A message</a>
           </article>                    
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,8 +1,29 @@
 import './footer.css'
-
+import {AiFillFacebook} from 'react-icons/ai';
+import {AiFillLinkedin} from 'react-icons/ai';
+import {AiFillGithub} from 'react-icons/ai';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='# ' className='footer__logo'>Mohamed Amine Ben Hmida</a>
+      <ul className='permalinks'>
+        <li><a href='# '>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href='https://www.facebook.com/medamine.benhamida'><AiFillFacebook/></a>
+        <a href='https://www.linkedin.com/in/mohamed-amine-ben-hmida-goldenboy/'><AiFillLinkedin/></a>
+        <a href='https://github.com/MohamedAminebenhmida'><AiFillGithub/></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; All rights are reserved.</small>
+      </div>
+    </footer>
   )
 }
 
